ed90875715378664f78ad735217babc58d2eb22d
Manny Espinoza <84434314+e-Manny@users.noreply.github.com>
2023-06-01 11:39:19 -0700

    added volunteeers and dragons APIs

--- dragonwatch_backend/app.js
+++ dragonwatch_backend/app.js
@@ -1,41 +1,46 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const analystsRouter = require('./routes/analysts');
+const analystsRouter = require("./routes/analysts");
+const volunteersRouter = require("./routes/volunteers");
+const dragonsRouter = require("./routes/dragons");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // api routes
-app.use('/api/analysts', analystsRouter);
+app.use("/api/analysts", analystsRouter);
+app.use("/api/volunteers", volunteersRouter);
+app.use("/api/dragons", dragonsRouter);
 
 // frontend
-app.use(express.static(path.join(__dirname, 'frontend')));
-app.get(/^\/(?!api).*$/, (req, res) => {  // catch non-api paths to make client-side routing work
-  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+app.use(express.static(path.join(__dirname, "frontend")));
+app.get(/^\/(?!api).*$/, (req, res) => {
+  // catch non-api paths to make client-side routing work
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
---
+++ dragonwatch_backend/routes/dragons.js
@@ -0,0 +1,93 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../db.js");
+
+// get all dragons
+router.get("/", (_req, res) => {
+  db.query("SELECT * FROM Dragons", (err, rows, fields) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+// get one dragon
+router.get("/:id", (req, res) => {
+  console.log(req.body.id);
+  db.query(
+    "SELECT * FROM Dragons WHERE id = ?",
+    [req.params.id],
+    (err, rows, fields) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        console.log(rows);
+        res.json(rows[0]);
+      }
+    }
+  );
+});
+
+// create dragon
+router.post("/", (req, res) => {
+  const id = req.body.id;
+  const nickame = req.body.nickame;
+  const sex = req.body.sex;
+  const color = req.body.color;
+  const length = req.body.length;
+  const wingspan = req.body.wingspan;
+  const hatch_year = req.body.hatch_year;
+  const identifying_marks = req.body.identifying_marks;
+  db.query(
+    "INSERT INTO Dragons (nickame, sex, color, length, wingspan, hatch_year, identifying_marks) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [id, nickame, sex, color, length, wingspan, hatch_year, identifying_marks],
+    (err, rows, fields) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.status(201).send();
+      }
+    }
+  );
+});
+
+// update dragon
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const nickame = req.params.nickame;
+  const sex = req.params.sex;
+  const color = req.params.color;
+  const length = req.params.length;
+  const wingspan = req.params.wingspan;
+  const hatch_year = req.params.hatch_year;
+  const identifying_marks = req.params.identifying_marks;
+  db.query(
+    "UPDATE Dragons SET nickame = ?, sex = ?, color = ?, length = ?, wingspan = ?, hatch_year = ?, identifying_marks = ?, WHERE id = ?",
+    [nickame, sex, color, length, wingspan, hatch_year, identifying_marks, id],
+    (err, rows, fields) => {
+      if (err) {
+        res.status(500).send(err.message);
+      } else {
+        res.status(204).send();
+      }
+    }
+  );
+});
+
+// delete dragon
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  db.query("DELETE FROM Dragons WHERE id = ?", [id], (err, rows, fields) => {
+    if (err) {
+      res.status(500).send(err.message);
+      return;
+    } else {
+      res.status(204).send();
+    }
+  });
+});
+
+module.exports = router;
