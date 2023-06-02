589b8ef27ab771c633efa8675f3766a34a6dcc5b
Maxwell DeKock <dekockm@oregonstate.edu>
2023-06-01 19:55:19 -0700

    Use correct host for database

--- dragonwatch_backend/db.js
+++ dragonwatch_backend/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: 'classmysql.engr.oregonstate.edu',
     user: 'cs340_dekockm',
     password: '0169', // this is bad, don't do this in production lol
     database: 'cs340_dekockm'
