80ea6b5a0b6988b7d3f0b1e9d127b821bc161126
Maxwell DeKock <dekockm@oregonstate.edu>
2023-06-11 09:26:08 -0700

    Remove DB password from source

--- dragonwatch_backend/db.js
+++ dragonwatch_backend/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const pool = mysql.createPool({
-    host: 'classmysql.engr.oregonstate.edu',
-    user: 'cs340_dekockm',
-    password: '0169', // this is bad, don't do this in production lol
-    database: 'cs340_dekockm'
+    host: process.env.DBHOST || 'classmysql.engr.oregonstate.edu',
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWD,
+    database: process.env.DBDATABASE
 });
 
 module.exports = pool;
